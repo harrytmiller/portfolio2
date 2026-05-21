d749219278b2c79accccb92b875e076114bd86ae
harrytmiller <harrytmiller42@gmail.com>
2026-05-21 01:59:26 +0100

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  images: { unoptimized: true },
+  output: 'export',
+  basePath: '/portfolio2',
+  assetPrefix: '/portfolio2',
+  images: {
+    unoptimized: true,
+  },
 };
+
 module.exports = nextConfig;
