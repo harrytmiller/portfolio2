9ce57a42741a84d24e561d806199732e2582ecee
Harry Miller <harrytmiller42@gmail.com>
2026-07-25 21:19:15 +0100

    Move project to repo root

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import "../styles/globals.css";
 import Layout from "../components/Layout";
 import { ThemeProvider } from "../context/ThemeContext";
@@ -5,6 +6,12 @@ import { ThemeProvider } from "../context/ThemeContext";
 export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider>
+      <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/icon-192.png" type="image/png" sizes="192x192" />
+        <link rel="icon" href="/icon-512.png" type="image/png" sizes="512x512" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
